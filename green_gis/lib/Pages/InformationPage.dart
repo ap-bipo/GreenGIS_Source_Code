41a31766fcaf54756a79782606d60241aa421b7b
Nguyễn An Phước <nguyenanphuoc@Air-cua-Nguyen.mshome.net>
2026-01-23 09:35:01 +0700

    Test 23/01/2026

---
+++ green_gis/lib/Pages/InformationPage.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class InforamtionPage extends StatefulWidget {
+  const InforamtionPage({super.key});
+
+  @override
+  State<InforamtionPage> createState() => _InforamtionPageState();
+}
+
+class _InforamtionPageState extends State<InforamtionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
